27eaba75a488c5fd549dc69fa8dc8e1c75acfd4c
Peeke Kuepers <pkuepers@kaliber.net>
2023-02-16 14:06:30 +0100

    Prevent re-installing effect on re-render when start and end are new objects with the same values

--- src/react.js
+++ src/react.js
@@ -12,20 +12,48 @@ export function useScrollProgression({
   const onChangeRef = React.useRef(null)
   onChangeRef.current = onChange
 
+  const stableStart = useStableObject(start)
+  const stableEnd = useStableObject(end)
+
   React.useEffect(
     () => {
       if (!node) return
       
       return onScrollProgression({
         element: node, 
-        start, 
-        end, 
+        start: stableStart, 
+        end: stableEnd, 
         clamp,
         onChange(...args) { onChangeRef.current(...args) }
       })
     },
-    [node, start, end, clamp]
+    [node, stableStart, stableEnd, clamp]
   )
 
   return setNode
+}
+
+function useStableObject(object) {
+  const objectRef = React.useRef(object)
+  
+  if (!isDeepCopy(object, objectRef.current)) {
+    objectRef.current = object 
+  }
+
+  return objectRef.current
+}
+
+function isDeepCopy(source, target) {
+  if(typeof source !== typeof target) return false
+  
+  if(Array.isArray(source) && Array.isArray(target)){
+    if(source.length !== target.length) return false
+    return source.every((value, index) => isDeepCopy(value, target[index]))
+  }
+  
+  if(typeof source === 'object' && typeof target === 'object') {
+    return Object.keys(source).every((key) => isDeepCopy(source[key], target[key]))
+  }
+  
+  return source === target
 }
